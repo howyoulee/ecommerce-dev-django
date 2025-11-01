f593f9016c08bde2529a1333fe20472ec345e12d
howyoulee <apandaseeds@foxmail.com>
2025-11-01 15:19:37 -0700

    1101

--- dev/cart/cart.py
+++ dev/cart/cart.py
@@ -11,4 +11,13 @@ class Cart():
 
         self.cart = cart
     
-    
+    def add(self, product, product_qty):
+        
+        product_id = str(product_id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
+
+        self.session.modified = True
